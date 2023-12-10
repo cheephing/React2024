d7585322fec846dcde2e680bc688e72359091ae3
calvinNg09 <ngcaprice@gmail.com>
2023-12-11 02:18:47 +0800

    check in

---
+++ final-1-after-setup/src/services/apiCabin.js
@@ -0,0 +1,20 @@
+import supabase from "./supabase";
+
+export default async function getCabins() {
+  const { data, error } = await supabase.from("cabins").select("*");
+
+  if (error) {
+    console.error(error);
+    throw new Error("cabins could not loaded");
+  }
+  return data;
+}
+
+export async function deleteCabin(id) {
+  const { data, error } = await supabase.from("cabins").delete().eq("id", id);
+  if (error) {
+    console.error(error);
+    throw new Error("cabins could not loaded");
+  }
+  return data;
+}
